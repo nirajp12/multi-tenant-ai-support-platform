62393dd80aa1b8388ccf8771cfd6eeaabef056be
Niraj Panchal <120939561+nirajp12@users.noreply.github.com>
2026-06-08 00:10:09 +0530

    day 1 task - (partial commit)

---
+++ aisupp/src/main/java/com/project/aisupp/AisuppApplication.java
@@ -0,0 +1,13 @@
+package com.project.aisupp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisuppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisuppApplication.class, args);
+	}
+
+}
---
+++ aisupp/src/test/java/com/project/aisupp/AisuppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.aisupp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisuppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
